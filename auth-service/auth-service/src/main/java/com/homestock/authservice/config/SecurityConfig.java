d1b6115a18852b8caad919bded2e6c44614bc4f6
Gabriel Moreira <gfmoreira.dev@gmail.com>
2025-11-07 23:42:17 +0000

    Implement JWT Authentication and Login route
    -Configured JWT Authentication and refresh tokens
    -Created Login and Refresh routes

--- auth-service/auth-service/src/main/java/com/homestock/authservice/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/homestock/authservice/config/SecurityConfig.java
@@ -1,24 +1,42 @@
 package com.homestock.authservice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // desativa CSRF (ok para APIs)
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().permitAll()
                 )
-                .httpBasic(httpBasic -> httpBasic.disable()) // desativa autenticação básica
-                .formLogin(form -> form.disable()); // desativa formulário de login
 
-        return http.build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authenticationProvider(authenticationProvider)
+
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
     }
 }
--- auth-service/auth-service/src/main/java/com/homestock/authservice/controller/UserController.java
+++ auth-service/auth-service/src/main/java/com/homestock/authservice/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.homestock.authservice.controller;
 
-import com.homestock.authservice.dto.UserCreateDto;
-import com.homestock.authservice.dto.UserDto;
+import com.homestock.authservice.dto.*;
 import com.homestock.authservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/auth")
@@ -14,8 +15,18 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody UserLoginDto request) {
+        return userService.login(request);
+    }
+
     @PostMapping("/register")
     public UserDto register(@RequestBody UserCreateDto request) {
         return userService.register(request);
+    }
+
+    @PostMapping("/refresh")
+    public String refresh(@RequestBody RefreshRequest request) {
+        return userService.refresh(request);
     }
 }
